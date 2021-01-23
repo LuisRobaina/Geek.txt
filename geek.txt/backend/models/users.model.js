ec4494b4d1b046493ad7fcaffa248a279c30d202
Luis Robaina <luisrobaina@Luiss-Air.attlocal.net>
2021-01-23 17:08:04 -0500

    Initial Structuring

--- geek.txt/backend/models/users.model.js
+++ geek.txt/backend/models/users.model.js
@@ -1,10 +1,21 @@
 // This is a base for the user model.
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     // Features
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5
+    },
     username: {
         // Validations for username.
         type: String,
@@ -14,7 +25,7 @@ const userSchema = new Schema({
         minlength: 3 // At least 3 chars for usernames.
     },
     // password ??
-    username: {
+    password: {
         // Validations for username.
         type: String,
         required: true,
@@ -22,11 +33,15 @@ const userSchema = new Schema({
         minlength: 5 // At least 5 chars for passwords.
     },
     // More stuff ??
-    
 },  {
     // Create a timestamps for the creation and modification of document.
     timestamps: true,
 });
+
+// Instance method to get initials of a user.
+userSchema.methods.getInitials = function() {
+    return this.firstName[0] + this.lastName[0]
+  }
 
 // Export userSchema as User.
 const User = mongoose.model('User', userSchema)
