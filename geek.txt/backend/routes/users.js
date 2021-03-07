a94dbe36594bb340873a83630505d4a705f660ea
Luis Robaina <luisfabian1545@gmail.com>
2021-03-06 19:45:19 -0500

    Fixed some bugs

--- geek.txt/backend/routes/users.js
+++ geek.txt/backend/routes/users.js
@@ -30,39 +30,37 @@ router.route('/login').post((req, res) => {
     const credential = req.body.credential;
     const password = req.body.password;
     
+    // Login by email.
     if(EmailValidator.validateEmail(credential)){
-
-        Users.find({email: credential}) 
-
-        .then(users => {
-            
-            bcrypt.compare(password, hash, function(err, valid) {
-
-                if(!valid) return res.json("Invalid");
-                if(err) return err;
-
-                var token = jwt.sign({ foo: 'bar' }, 'shhhhh'); //?????
-
-            });
-            res.json(users)
+        Users.findOne({email: credential}) 
+        .then(user => {
+            if(!user){
+                return res.status(400).json('Invalid Credentials')
+            }
+            result = bcrypt.compareSync(password, user.password);
+            if(result){
+                return res.json(user)
+            }
+            else{
+                return res.status(400).json('Invalid Credentials')
+            }
         })
-
         .catch(err => res.status(400).json('Error: ' + err))
     } else {
         // Look user by GeekID.
-        Users.find({geekID: credential}) 
+        Users.findOne({geekID: credential}) 
         // If promise return then return all users as JSON.
-        .then(users => {
-            bcrypt.compare(password, hash, function(err, valid) {
-
-                if(!valid) return res.json("Invalid");
-                if(err) return err;
-
-                var token = jwt.sign({ foo: 'bar' }, 'shhhhh'); //?????
-
-            });
-            
-            res.json(users)
+        .then(user => {
+            if(!user){
+                return res.status(400).json('Invalid Credentials')
+            }
+            result = bcrypt.compareSync(password, user.password);
+            if(result){
+                return res.json(user)
+            }
+            else{
+                return res.status(400).json('Invalid Credentials')
+            }
         })
         // If there is an error return status 400 with Error.   check if password  matches hashed in DB, return error if not
         .catch(err => res.status(400).json('Error: ' + err))
@@ -84,35 +82,33 @@ router.route('/add').post((req, res) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
-    const password = req.body.password;
+    password = req.body.password;
     const creditCard = []
     const shippingAddress = []
     const wishList = []
 
     bcrypt.hash(password, saltRounds, function(err, hash) {
 
-    if(err) return res.status(400);
-
-    // Create new user using the User model.
-    const newUser = new Users({
-        geekID,
-        firstName,
-        lastName,
-        email,
-        password,
-        creditCard,
-        shippingAddress,
-        wishList
+        if(err) return res.status(400);
+        password = hash;
+        // Create new user using the User model.
+        const newUser = new Users({
+            geekID,
+            firstName,
+            lastName,
+            email,
+            password,
+            creditCard,
+            shippingAddress,
+            wishList
     });
 
     // Save new user to database. 
     newUser.save()
         .then(() => res.json('User Added Successfully.'))
         .catch(err => res.status(400).json('Error: ' + err))
-
     });
 
 });
-
 // Export all User routes in this routers object.
 module.exports = router
