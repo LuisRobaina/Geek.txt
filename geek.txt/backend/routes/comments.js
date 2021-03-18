50c120fef8168803ff7a2246908e69c8520f8be5
Luis Robaina <luisfabian1545@gmail.com>
2021-03-17 20:27:51 -0400

    Fixed the bug that was making the comments backend code not work. The import statement was incorrect

--- geek.txt/backend/routes/comments.js
+++ geek.txt/backend/routes/comments.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 let Books = require('../models/books.model')
 
 const router = require('express').Router();
-let Comment = require('../models/CommentingModels/ratings.model')
+let Comment = require('../models/CommentingModels/comments.model')
 
 // Handles incomming GET requests to url/comments/ .
 router.route('/').get((req, res) => {
