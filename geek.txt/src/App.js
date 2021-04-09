3042d93766cae05f193357a0529194d542e08309
Logan Simijoski <logansimi@gmail.com>
2021-04-09 15:45:37 -0400

    Added WIP frontend pages for user profie, add card, add address, edit profile, edit card, and edit address

--- geek.txt/src/App.js
+++ geek.txt/src/App.js
@@ -7,7 +7,13 @@ import Home from './views/Home';
 import Book from './views/Book';
 import NotFound from './views/NotFound';
 import UserLogIn from './views/UserLogIn';
-import UserRegister from './views/UserRegister';
+import UserRegister from './views/UserRegister'; 
+import UserProfilePage from './views/UserProfilePage';
+import AddNewCard from './views/AddNewCard';
+import AddNewAddress from './views/AddNewAddress';
+import EditProfile from './views/EditProfile';
+import EditCard from './views/EditCard';
+import EditAddress from './views/EditAddress';
 
 // Example of component routing:
 // <Route path="/" exact component={BookBrowser} />
@@ -21,6 +27,12 @@ const App = () => {
         <Route path="/register" exact component={UserRegister} />
         <Route path="/books/:id" exact component={Book} />
         <Route path="/" exact component={Home} />
+        <Route path="/profile" exact component={UserProfilePage}/>
+        <Route path="/addcard" component={AddNewCard} />
+        <Route path="/addaddress" component={AddNewAddress} />
+        <Route path="/editprofile" component={EditProfile} />
+        <Route path="/editcards" component={EditCard} />
+        <Route path="/editaddresses" component={EditAddress} />
         <Route path="/*" component={NotFound} />
       </Switch>
     </Router>
---
+++ geek.txt/src/views/EditCard.js
@@ -0,0 +1,10 @@
+//import React, { useState } from 'react';
+
+const EditCard = () => {
+  return (
+    <div>
+      <p>EDIT CARD</p>
+    </div>
+  );
+};
+ export default EditCard;
